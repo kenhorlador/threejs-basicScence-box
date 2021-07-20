5952392e5965dc51872c1ce9601acb1f7a3a5f36
Ken Horlador <kenhorlador@gmail.com>
2021-07-21 00:35:51 +0800

    change camera perspective

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.x = 1.2
+camera.position.y = 1.2
 scene.add( camera ) 
 
 
